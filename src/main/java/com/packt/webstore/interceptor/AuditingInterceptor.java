9875df04b7474fd419918de7a0ac165380b1c51a
HChan <hchanenator@gmail.com>
2015-09-21 09:19:44 -0400

    Add simple audit logging on add product page

---
+++ src/main/java/com/packt/webstore/interceptor/AuditingInterceptor.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package com.packt.webstore.interceptor;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * @author herb
+ *
+ */
+public class AuditingInterceptor extends HandlerInterceptorAdapter {
+	
+	Logger logger = Logger.getLogger("auditLogger");
+	private String user;
+	private String productId;
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		if (request.getRequestURI().endsWith("products/add") && request.getMethod().equals("POST")) {
+			user = request.getRemoteUser();
+			productId = request.getParameterValues("productId")[0];
+		}
+		
+		return true;
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		if (request.getRequestURI().endsWith("products/add") && response.getStatus() == 302) {
+			logger.info(String.format("A new product [%s] added by %s on %s", productId, user, getCurrentTime()));			
+		}
+	}
+
+	private String getCurrentTime() {
+		DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy 'at' hh:mm:ss");
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(System.currentTimeMillis());
+		
+		return formatter.format(calendar.getTime());
+	}
+	
+	
+	
+	
+
+}
